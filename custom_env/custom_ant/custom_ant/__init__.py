148a1e8d85d854dad958a92e6d653b1a994800b9
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2020-11-22 12:53:42 -0800

    Added a second environment that is identical to ant_v3.py from openai gym

--- custom_env/custom_ant/custom_ant/__init__.py
+++ custom_env/custom_ant/custom_ant/__init__.py
@@ -4,3 +4,7 @@ register(id='CustomAnt-v0', # id by which to refer to the new environment; the s
 	entry_point='custom_ant.envs:CustomAntEnv' # points to the class that inherits from gym.Env and defines the four basic functions, i.e. reset, step, render, close
 )
 
+
+register(id='CustomAnt-v2', # id by which to refer to the new environment; the string is passed as an argument to gym.make() to create a copy of the environment
+	entry_point='custom_ant.envs:CustomAntEnvV2' # points to the class that inherits from gym.Env and defines the four basic functions, i.e. reset, step, render, close
+)
--- custom_env/custom_ant/custom_ant/envs/__init__.py
+++ custom_env/custom_ant/custom_ant/envs/__init__.py
@@ -1,1 +1,2 @@
 from custom_ant.envs.custom_ant_env import CustomAntEnv
+from custom_ant.envs.custom_ant_env_v2 import CustomAntEnvV2
