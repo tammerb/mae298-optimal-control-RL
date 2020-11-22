0342cc9823a3705885bf7f2381a35f0d657a2cf0
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2020-11-21 17:38:13 -0800

    Add files via upload

---
+++ custom_env/custom_ant/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='custom_envs', # name of the package
+	version='0.0.1', # version of this release
+	install_requires=['gym', 'mujoco_py'] # specifies the minimal list of libraries required to run the package correctly
+)
+
