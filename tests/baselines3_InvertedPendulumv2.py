37827f78b7d53512fcf06958644565703ddd832e
tammerb <tammer.barkouki@gmail.com>
2020-12-01 23:20:34 -0800

    Run Optuna on InvertedPendulum and train a model on optimized parameters

--- tests/baselines3_InvertedPendulumv2.py
+++ tests/baselines3_InvertedPendulumv2.py
@@ -1,50 +1,39 @@
 import gym
+import os
 
-from stable_baselines3 import PPO
 from stable_baselines3 import A2C
 from stable_baselines3.common.evaluation import evaluate_policy
+
+TOTAL_TIMESTEPS = 20000
 
 # Create environment
 env = gym.make('InvertedPendulum-v2')
 
-# Instantiate and train the agent
-# model = A2C('MlpPolicy', env, 
-#         learning_rate = 7e-4,
-#         n_steps = 5,
-#         gamma = 0.99,
-#         gae_lambda = 1.0,
-#         ent_coef = 0.0,
-#         vf_coef = 0.5,
-#         max_grad_norm = 0.5,
-#         rms_prop_eps = 1e-5,
-#         verbose=1
-#         ).learn(10000)
-model = PPO('MlpPolicy', env,
-        learning_rate = 3e-4,
-        n_steps = 2048,
-        batch_size = 64,
-        n_epochs = 10,
-        gamma = 0.99,
-        gae_lambda = 0.95,
-        clip_range = 0.2,
-        clip_range_vf = None,
-        ent_coef = 0.0,
+# Instantiate the agent
+optuna = True
+
+if optuna:
+  print("Optuna = True")
+  file = "/saved_models/optuna_InvertedPendulum"
+  model = A2C('MlpPolicy', env, 
+        gamma = 0.9998711578038559,
+        n_steps = 1024,
         vf_coef = 0.5,
-        max_grad_norm = 0.5,
+        ent_coef = 0.03137755757220838,
+        max_grad_norm = 0.33124753710629307,
+        learning_rate = 0.010249423072591952,
+        gae_lambda = 0.8572939586269473,       
+        rms_prop_eps = 1e-5,
         verbose=1
-        ).learn(40000)
+        )
+else:
+  print("Optuna = False")
+  file = "/saved_models/default_InvertedPendulum"
+  model = A2C('MlpPolicy', env, verbose=1)
 
-# Evaluate the trained agent
-mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10, deterministic=True)
-print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
-
-# Render the trained agent
-obs = env.reset()
-for i in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
-    obs, rewards, done, info = env.step(action)
-    env.render()
-    if done:
-      obs = env.reset()
+# Train the agent
+model.learn(total_timesteps=TOTAL_TIMESTEPS)
+model.save(os.getcwd() + file)
 
 env.close()
+
--- tests/baselines3_cartpole.py
+++ tests/baselines3_cartpole.py
@@ -4,66 +4,35 @@ import os
 from stable_baselines3 import A2C
 from stable_baselines3.common.evaluation import evaluate_policy
 
-TOTAL_TIMESTEPS = 10000
-NUM_EVAL_EPIS = 1000
+TOTAL_TIMESTEPS = 20000
 
 # Create environment
 env = gym.make('CartPole-v1')
 
 # Instantiate the agent
-model1 = A2C('MlpPolicy', env, 
-        learning_rate = 7e-4,
-        n_steps = 5,
-        gamma = 0.99,
-        gae_lambda = 1.0,
-        ent_coef = 0.0,
-        vf_coef = 0.5,
-        max_grad_norm = 0.5,
-        rms_prop_eps = 1e-5,
-        verbose=0
-        )
+optuna = False
 
-model2 = A2C('MlpPolicy', env, 
-        learning_rate = 0.0015613269950806646,
-        n_steps = 256,
+if optuna:
+  print("Optuna = True")
+  file = "/saved_models/optuna_cartpole"
+  model = A2C('MlpPolicy', env, 
         gamma = 0.9987940879356862,
-        gae_lambda = 0.994637205730802,
-        ent_coef = 1.2954655366837278e-06,
+        n_steps = 256,
         vf_coef = 0.5,
-        max_grad_norm = 1.92012534215678,
+        ent_coef = 1.2954655366837278e-06,
+        max_grad_norm = 0.7111167513074961,
+        learning_rate = 0.0015613269950806646,
+        gae_lambda = 0.994637205730802,       
         rms_prop_eps = 1e-5,
-        verbose=0
+        verbose=1
         )
+else:
+  print("Optuna = False")
+  file = "/saved_models/default_cartpole"
+  model = A2C('MlpPolicy', env, verbose=1)
 
 # Train the agent
-print("Training default model")
-model1.learn(total_timesteps=TOTAL_TIMESTEPS)
-model1.save(os.getcwd() + "/default_cartpole")
-print("Training optuna model")
-model2.learn(total_timesteps=TOTAL_TIMESTEPS)
-model2.save(os.getcwd() + "/optuna_cartpole")
-
-# Evaluate the trained agent
-print("Evaluating the models")
-mean_reward1, std_reward1 = evaluate_policy(model1, env, n_eval_episodes=NUM_EVAL_EPIS, deterministic=True)
-mean_reward2, std_reward2 = evaluate_policy(model2, env, n_eval_episodes=NUM_EVAL_EPIS, deterministic=True)
-print(f"Default model: mean_reward={mean_reward1:.2f} +/- {std_reward1}")
-print(f"Optuna model: mean_reward={mean_reward2:.2f} +/- {std_reward2}")
-
-# Render the trained agent
-obs = env.reset()
-for i in range(500):
-    action, _states = model1.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
-    env.render()
-    if done:
-      obs = env.reset()
-obs = env.reset()
-for i in range(500):
-    action, _states = model2.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
-    env.render()
-    if done:
-      obs = env.reset()
+model.learn(total_timesteps=TOTAL_TIMESTEPS)
+model.save(os.getcwd() + file)
 
 env.close()
