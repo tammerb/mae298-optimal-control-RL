c4bcf553f34cbc70ee47a6254b62b329ef365bd8
Tammer Barkouki <tammer@TheBlackPearl.HRVIP>
2020-12-03 00:00:06 -0800

    make model training a function and run both sb3 and optuna in one go

--- tests/compare_Antv2.py
+++ tests/compare_Antv2.py
@@ -9,41 +9,39 @@ TOTAL_TIMESTEPS = 1e6
 # Create environment
 env = gym.make('Ant-v2')
 
-# Instantiate the agent
-optuna = True
+def train_model(optuna):
+  if optuna:
+    print("Optuna = True")
+    file = "/saved_models/optuna_Ant"
+    model = A2C('MlpPolicy', env, 
+          gamma = 0.9980152091572712,
+          n_steps = 256,
+          vf_coef = 0.5,
+          ent_coef = 1.7157437409384092e-06,
+          max_grad_norm = 0.3598150713374998,
+          learning_rate = 7.60205316312803e-05,
+          gae_lambda = 0.942374289947569,       
+          rms_prop_eps = 1e-5,
+          verbose=0
+          )
+  else:
+    print("Optuna = False")
+    file = "/saved_models/default_Ant"
+    model = A2C('MlpPolicy', env, 
+          gamma = 0.99,
+          n_steps = 5,
+          vf_coef = 0.5,
+          ent_coef = 0.0,
+          max_grad_norm = 0.5,
+          learning_rate = 0.0007,
+          gae_lambda = 1.0,       
+          rms_prop_eps = 1e-5,
+          verbose=0
+          )
+  # Train the agent
+  model.learn(total_timesteps=TOTAL_TIMESTEPS)
+  model.save(os.getcwd() + file)
+  env.close()
 
-if optuna:
-  print("Optuna = True")
-  file = "/saved_models/optuna_Ant"
-  model = A2C('MlpPolicy', env, 
-        gamma = 0.9980152091572712,
-        n_steps = 256,
-        vf_coef = 0.5,
-        ent_coef = 1.7157437409384092e-06,
-        max_grad_norm = 0.3598150713374998,
-        learning_rate = 7.60205316312803e-05,
-        gae_lambda = 0.942374289947569,       
-        rms_prop_eps = 1e-5,
-        verbose=0
-        )
-else:
-  print("Optuna = False")
-  file = "/saved_models/default_Ant"
-  model = A2C('MlpPolicy', env, 
-        gamma = 0.99,
-        n_steps = 5,
-        vf_coef = 0.5,
-        ent_coef = 0.0,
-        max_grad_norm = 0.5,
-        learning_rate = 0.0007,
-        gae_lambda = 1.0,       
-        rms_prop_eps = 1e-5,
-        verbose=0
-        )
-
-# Train the agent
-model.learn(total_timesteps=TOTAL_TIMESTEPS)
-model.save(os.getcwd() + file)
-
-env.close()
-
+train_model(False)
+train_model(True)
