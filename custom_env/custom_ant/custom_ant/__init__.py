e9cf721243174e24d924c1fb6cc05a0b73af0273
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2020-12-05 19:44:23 -0800

    Added clean Ant env as custom_ant_en_v4

--- custom_env/custom_ant/custom_ant/__init__.py
+++ custom_env/custom_ant/custom_ant/__init__.py
@@ -12,3 +12,7 @@ register(id='CustomAnt-v2', # id by which to refer to the new environment; the s
 register(id='CustomAnt-v3', # id by which to refer to the new environment; the string is passed as an argument to gym.make() to create a copy of the environment
 	entry_point='custom_ant.envs:CustomAntEnvV3' # points to the class that inherits from gym.Env and defines the four basic functions, i.e. reset, step, render, close
 )
+
+register(id='CustomAnt-v4', # id by which to refer to the new environment; the string is passed as an argument to gym.make() to create a copy of the environment
+	entry_point='custom_ant.envs:CustomAntEnvV4' # points to the class that inherits from gym.Env and defines the four basic functions, i.e. reset, step, render, close
+)
--- custom_env/custom_ant/custom_ant/envs/__init__.py
+++ custom_env/custom_ant/custom_ant/envs/__init__.py
@@ -1,3 +1,4 @@
 from custom_ant.envs.custom_ant_env_v0 import CustomAntEnvV0
 from custom_ant.envs.custom_ant_env_v2 import CustomAntEnvV2
 from custom_ant.envs.custom_ant_env_v3 import CustomAntEnvV3
+from custom_ant.envs.custom_ant_env_v4 import CustomAntEnvV4
