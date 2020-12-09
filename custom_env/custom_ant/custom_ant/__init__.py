6222436493b0c58d659f804137e78ff0aa75dc5c
texmat <tmatthews@ucdavis.edu>
2020-12-08 21:10:16 -0800

    New Env for target seeking block model

--- custom_env/custom_ant/custom_ant/__init__.py
+++ custom_env/custom_ant/custom_ant/__init__.py
@@ -24,3 +24,7 @@ register(id='Block-v0', # id by which to refer to the new environment; the strin
 register(id='Block-v1', # id by which to refer to the new environment; the string is passed as an argument to gym.make() to create a copy of the environment
 	entry_point='custom_ant.envs:BlockV1' # points to the class that inherits from gym.Env and defines the four basic functions, i.e. reset, step, render, close
 )
+
+register(id='Block-v2', # id by which to refer to the new environment; the string is passed as an argument to gym.make() to create a copy of the environment
+	entry_point='custom_ant.envs:BlockV2' # points to the class that inherits from gym.Env and defines the four basic functions, i.e. reset, step, render, close
+)
--- custom_env/custom_ant/custom_ant/envs/__init__.py
+++ custom_env/custom_ant/custom_ant/envs/__init__.py
@@ -4,3 +4,4 @@ from custom_ant.envs.custom_ant_env_v3 import CustomAntEnvV3
 from custom_ant.envs.custom_ant_env_v4 import CustomAntEnvV4
 from custom_ant.envs.block_env_v0 import BlockV0
 from custom_ant.envs.block_env_v1 import BlockV1
+from custom_ant.envs.block_env_v2 import BlockV2
