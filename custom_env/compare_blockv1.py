473694e33dd39ce356baa7216c886c69b6ff1b26
tammerb <tammer.barkouki@gmail.com>
2020-12-09 00:08:44 -0800

    create bestballback.py with BestCallBack class

--- custom_env/compare_blockv1.py
+++ custom_env/compare_blockv1.py
@@ -1,19 +1,43 @@
-import custom_ant
+
 import gym
 import os
+import custom_ant
+import bestcallback
+import numpy as np
+import matplotlib.pyplot as plt
 
 from stable_baselines3 import A2C
 from stable_baselines3.common.evaluation import evaluate_policy
 
+from stable_baselines import results_plotter
+from stable_baselines.bench import Monitor
+from stable_baselines.results_plotter import load_results, ts2xy
+from stable_baselines.common.noise import AdaptiveParamNoiseSpec
+from stable_baselines.common.callbacks import BaseCallback
+
+
 TOTAL_TIMESTEPS = 2e6
 
+env_name='Block-v1'
+models_dir = 'saved_models/'
+
+os.makedirs(models_dir, exist_ok=True)
+
 # Create environment
-env = gym.make('Block-v1')
+# env = gym.make(env_name)
 
 def train_model(optuna):
+  env = gym.make(env_name)
+  
   if optuna:
     print("Optuna = True")
-    file = "/saved_models/optuna_blockv1"
+    prefix = "optuna_"
+    log_dir = prefix + '/'
+    os.makedirs(log_dir, exist_ok=True)
+    env = Monitor(env, log_dir)
+
+    file = models_dir + prefix + env_name
+    # env, callback = setup_callback(log_dir, env)
     model = A2C('MlpPolicy', env, 
           gamma = 0.993630753740229,
           n_steps = 32,
@@ -27,7 +51,13 @@ def train_model(optuna):
           )
   else:
     print("Optuna = False")
-    file = "/saved_models/default_blockv1"
+    prefix = "default_"
+    log_dir = prefix + '/'
+    os.makedirs(log_dir, exist_ok=True)
+    env = Monitor(env, log_dir)
+
+    file = models_dir + prefix + env_name
+    # env, callback = setup_callback(log_dir, env)
     model = A2C('MlpPolicy', env, 
           gamma = 0.99,
           n_steps = 5,
@@ -38,10 +68,14 @@ def train_model(optuna):
           gae_lambda = 1.0,       
           rms_prop_eps = 1e-5,
           verbose=1
-          )
+    )
+  
+  cb = bestcallback.BestCallback(check_freq=1000, log_dir=log_dir)
+ 
   # Train the agent
-  model.learn(total_timesteps=TOTAL_TIMESTEPS)
+  model.learn(total_timesteps=int(TOTAL_TIMESTEPS), callback=cb)
   model.save(os.getcwd() + file)
+  
   mean_reward, std_reward = eval(model)
   env.close()
   return mean_reward, std_reward
@@ -51,12 +85,21 @@ def eval(model):
   print("Evaluating the model")
   return evaluate_policy(model, env, n_eval_episodes=1000)
 
-def print_rewards(mean_reward, std_reward):
-  print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
+#def print_rewards(mean_reward, std_reward):
+#  print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
   
+def setup_callback(log_dir, env):
+  os.makedirs(log_dir, exist_ok=True)
+  env = Monitor(env, log_dir)
+  callback = BlockCallback(check_freq=1000, log_dir=log_dir)
+  return env, callback
+
 
 mean_reward1, std_reward1 = train_model(True)
 mean_reward2, std_reward2 = train_model(False)
 
 print_rewards(mean_reward1, std_reward1)
 print_rewards(mean_reward2, std_reward2)
+
+results_plotter.plot_results(["/optuna_"], TOTAL_TIMESTEPS, results_plotter.X_TIMESTEPS, "Block_v1")
+plt.show()
