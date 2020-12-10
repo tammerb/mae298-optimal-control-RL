8913ea233d357a86af0b18b67542c8b11aaabf91
tammerb <tammer.barkouki@gmail.com>
2020-12-09 22:17:14 -0800

    Store model and monitor i n a unique bag file, delete monitor if exists

--- custom_env/compare_blockv1.py
+++ custom_env/compare_blockv1.py
@@ -9,27 +9,31 @@ import matplotlib.pyplot as plt
 from stable_baselines3 import A2C
 from stable_baselines3.common.evaluation import evaluate_policy
 
-from stable_baselines import results_plotter
 from stable_baselines.bench import Monitor
-from stable_baselines.results_plotter import load_results, ts2xy
-from stable_baselines.common.noise import AdaptiveParamNoiseSpec
-from stable_baselines.common.callbacks import BaseCallback
+from stable_baselines3.common import results_plotter
+from stable_baselines3.common.results_plotter import load_results, ts2xy
+from stable_baselines3.common.callbacks import BaseCallback
 
 
-TOTAL_TIMESTEPS = 2e5
 
+
+
+TOTAL_TIMESTEPS = 2e6
 env_name='Block-v1'
-models_dir = 'saved_models/'
 
-os.makedirs(models_dir, exist_ok=True)
 
-def train_model(optuna, env):
+
+
+bag_dir = env_name + '_bag/'
+os.makedirs(bag_dir, exist_ok=True)
+
+def train_model(optuna, env, bag_dir):
   
   if optuna:
     print("Optuna = True")
-    prefix = "optuna"
-    log_dir = prefix + '/'
-    file = models_dir + prefix + env_name
+    prefix = "optuna/"
+    log_dir = bag_dir + prefix
+    model_dir = bag_dir + prefix
     env, callback = setup_callback(log_dir, env)
     model = A2C('MlpPolicy', env, 
           gamma = 0.993630753740229,
@@ -40,13 +44,13 @@ def train_model(optuna, env):
           learning_rate = 0.00021258581917570237,
           gae_lambda = 0.9973243722326772,       
           rms_prop_eps = 1e-5,
-          verbose=1
+          verbose=0
           )
   else:
     print("Optuna = False")
-    prefix = "default"
-    log_dir = prefix + '/'
-    file = models_dir + prefix + env_name
+    prefix = "default/"
+    log_dir = bag_dir + prefix
+    model_dir = bag_dir + prefix
     env, callback = setup_callback(log_dir, env)
     model = A2C('MlpPolicy', env, 
           gamma = 0.99,
@@ -57,14 +61,14 @@ def train_model(optuna, env):
           learning_rate = 0.0007,
           gae_lambda = 1.0,       
           rms_prop_eps = 1e-5,
-          verbose=1
+          verbose=0
     )
   
-  cb = PlotCallBack(check_freq=1000, log_dir=log_dir)
+  callback = PlotCallBack(check_freq=1000, log_dir=log_dir)
  
   # Train the agent
-  model.learn(total_timesteps=int(TOTAL_TIMESTEPS), callback=cb)
-  model.save(os.getcwd() + file)
+  model.learn(total_timesteps=int(TOTAL_TIMESTEPS), callback=callback)
+  model.save(os.getcwd() + model_dir)
   
   mean_reward, std_reward = eval(model)
   env.close()
@@ -73,27 +77,30 @@ def train_model(optuna, env):
 # Evaluate the trained agent
 def eval(model):
   print("Evaluating the model")
-  return evaluate_policy(model, env, n_eval_episodes=1000)
+  return evaluate_policy(model, env, n_eval_episodes=100)
 
 def print_rewards(mean_reward, std_reward):
   print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
   
 def setup_callback(log_dir, env):
+  if os.path.isfile(log_dir + 'monitor.csv'):
+    print("A monitor.csv already exists. Deleting it.")
+    os.remove(log_dir + 'monitor.csv')
+    if not os.path.isfile(log_dir + 'monitor.csv'):
+      print("Old monitor.csv successfully deleted")
   os.makedirs(log_dir, exist_ok=True)
   env = Monitor(env, log_dir)
   callback = PlotCallBack(check_freq=1000, log_dir=log_dir)
   return env, callback
 
 env = gym.make(env_name)
-mean_reward1, std_reward1 = train_model(False, env)
+mean_reward1, std_reward1 = train_model(False, env, bag_dir)
 env = gym.make(env_name)
-mean_reward2, std_reward2 = train_model(True, env)
+mean_reward2, std_reward2 = train_model(True, env, bag_dir)
 
 print_rewards(mean_reward1, std_reward1)
 print_rewards(mean_reward2, std_reward2)
 
 results_plotter.plot_results(["default/"], TOTAL_TIMESTEPS, results_plotter.X_TIMESTEPS, "Block_v1")
-plt.show()
-
-results_plotter.plot_results(["default/"], TOTAL_TIMESTEPS, results_plotter.X_TIMESTEPS, "Block_v1")
+results_plotter.plot_results(["optuna/"], TOTAL_TIMESTEPS, results_plotter.X_TIMESTEPS, "Block_v1")
 plt.show()
