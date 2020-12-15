9cdb4c26c3bfad0caf9baf24f1d6f54a589bc156
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2020-12-14 21:57:07 -0800

    Added Finalized Blockv2

---
+++ custom_env/Block-v2/plot.py
@@ -0,0 +1,18 @@
+PLOT_NAME = "Push Block to Goal Location"
+ENV_ID = 'Block-v2'
+bag_dir = 'bag/'
+TOTAL_TIMESTEPS = 5e5
+
+legend_names = []
+legend_names.append('Default')
+legend_names.append('Optuna')
+
+results = []
+results.append(bag_dir + "default/")
+results.append(bag_dir + "optuna/")
+from stable_baselines3.common import results_plotter
+import matplotlib.pyplot as plt
+
+results_plotter.plot_results(results, TOTAL_TIMESTEPS, results_plotter.X_TIMESTEPS, PLOT_NAME)
+plt.legend(legend_names)
+plt.show()
